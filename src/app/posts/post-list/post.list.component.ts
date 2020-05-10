8577003ee345ec16ca804715d1cf7b597e0d46ef
Subarna Karki <subarna.karki13@gmail.com>
2020-05-10 00:36:49 -0400

    add a model for posts

--- src/app/posts/post-list/post.list.component.ts
+++ src/app/posts/post-list/post.list.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Post } from '../post.model'
+
 
 @Component({
   selector: 'app-post-list',
@@ -7,7 +9,5 @@ import { Component, Input } from '@angular/core';
 })
 
 export class PostListComponent {
-  @Input() posts = [
-    
-  ]
+  @Input() posts: Post[] = [];
 }
